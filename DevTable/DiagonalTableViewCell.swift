f4690f0a79930aa437eee941c258d6f3af837c3d
Agapov Alex <agapov.one@gmail.com>
2016-07-15 15:53:51 +0500

    structure

---
+++ DevTable/DiagonalTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  DiagonalTableViewCell.swift
+//  DevTable
+//
+//  Created by Алексей Агапов on 15/07/16.
+//  Copyright © 2016 ru.testing.project. All rights reserved.
+//
+
+import UIKit
+import CoreGraphics
+
+class DiagonalTableViewCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+    
+    
+  }
+}
